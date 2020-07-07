948f8be26dc67e4e77d1c873896ddc7573456fc4
kthone <kthone@snu.ac.kr>
2020-07-07 23:08:15 +0900

    pix2pix model Generator Done... pix2pix model Discriminator Done...

--- Pix2Pix/models.py
+++ Pix2Pix/models.py
@@ -96,7 +96,25 @@ class GeneratorUNet(nn.Module):
 
 
 class Discriminator(nn.Module):
-    def __init__(self, in_chaneels = 3):
+    def __init__(self, in_channels = 3):
         super(Discriminator, self).__init__()
 
         def discriminator_block(in_filters, out_filters, normalization = True):
+            layers = [nn.Conv2d(in_filters, out_filters, 4, stride = 2, padding = 1)]
+            if normalization:
+                layers.append(nn.InstanceNorm2d(out_filters))
+            layers.append(nn.LeakyReLU(0.2 , inplace = True))
+            return layers
+
+        self.model = nn.Sequential(
+            *discriminator_block(in_channels*2, 64, normalization = False),
+            *discriminator_block(64, 128),
+            *discriminator_block(128, 256),
+            *discriminator_block(256. 512),
+        nn.ZeroPad2d((1, 0, 1, 0)),
+        nn.Conv2d(512, 1, 4, padding=1, bias=False)
+        )
+
+        def forward(self, img_A, img_B):
+            img_input = torch.cat((img_A, img_B), 1)
+            return self.model(img_input)
